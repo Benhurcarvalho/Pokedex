c0eda3b2d34047c0908df27b7994aca89b6a4c16
Ben-Hur Carvalho <benhurcarvalho2@gmail.com>
2023-04-11 15:06:55 -0300

    assemble the initial structure of the pokédex

---
+++ pokedex/src/components/Header.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className='header'>
+        <div className='balls'>
+            <div className="blue-ball"></div>
+            <div className="red-ball"></div>
+            <div className="yellow-ball"></div>
+            <div className="green-ball"></div>
+        </div>  
+    </div>
+  )
+}
